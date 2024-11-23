06ca2183b52d442becfb77712ed56ef3652f346f
sanelemvubu <sanelem@digikonect.co.za>
2024-11-23 19:09:43 +0200

    chore: initial commit

---
+++ Kata.Challange.Characters.Tests/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace Kata.Challange.Characters.Tests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
